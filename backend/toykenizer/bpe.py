6ad08bf7b11efaca99a791dbc1a7f178b5574801
Juan Munoz Corredor <jsmucor@gmail.com>
2025-12-13 16:54:36 -0800

    Fixed a bunch of stuff now we are minting new tokens based on the algorithm

--- backend/toykenizer/bpe.py
+++ backend/toykenizer/bpe.py
@@ -1,39 +1,57 @@
+# Imports
+from os import replace
+import sys
 
-# Import and encode text into the UTF-8 byte representation
-file = open ('text.txt', 'r')
-content = file.read()
-tokens = content.encode('utf-8')
-tokens = list(map(int, tokens))
+# Constants
+PATH_TO_FILE = "text.txt"
 
-def most_common_pair( ids: list[int] ):
+
+# Read file and encode text into the UTF-8 bytes
+def get_tokens(path: str):
+    file = open(path, "r")
+    content = file.read()
+    tokens = content.encode("utf-8")
+    return list(map(int, tokens))
+
+
+# Find the most common pair out of a list of utf-8 character ids
+def most_common_pair(tokens: list[int]):
     freq_counts = {}
-    for num in ids:
-        if num not in freq_counts:
-            freq_counts[num] = 0
-        freq_counts[num] += 1
-    freq_count_list = list(freq_counts.items())
+    for pair in zip(tokens, tokens[1:]):
+        if pair in freq_counts:
+            freq_counts[pair] += 1
+        else:
+            freq_counts[pair] = 1
+    freq_counts = dict(
+        sorted(freq_counts.items(), key=lambda item: item[1], reverse=True)
+    )
+    return next(iter(freq_counts))
 
-    quicksort_tuples( freq_count_list, 0, len( freq_count_list ) - 1)
 
-    return (freq_count_list)
+# Mint the new token based on the most common pair
+def mint_token(tokens: list[int], pair_to_replace: tuple[int, int]):
+    new_token = max(tokens + [255]) + 1
+    new_tokens = []
 
-def partition( tuples: list[tuple[int, int]], low: int, high: int) -> int:
-    pivot = tuples[high]
-    i = low -1
-
-    for j in range(low, high):
-        if tuples[j][1] >= pivot [1]:
+    i = 0
+    while i < len(tokens):
+        if i + 1 < len(tokens) and (tokens[i], tokens[i + 1]) == pair_to_replace:
+            new_tokens.append(new_token)
+            i += 2
+        else:
+            new_tokens.append(tokens[i])
             i += 1
-            tuples[i], tuples[j] = tuples[j], tuples[i]
 
-    tuples[i+1], tuples[high] = tuples[high], tuples[i+1]
+    return new_tokens
 
-    return i+1
 
-def quicksort_tuples( tuples: list[tuple[int,int]], low:int, high:int):
-    if low < high:
-        initial_partition = partition( tuples, low, high )
-        quicksort_tuples( tuples, low, initial_partition-1 )
-        quicksort_tuples( tuples, initial_partition+1, high )
+# Main function
+def main():
+    tokens = get_tokens(PATH_TO_FILE)
+    common_pair = most_common_pair(tokens)
+    print(mint_token(tokens, common_pair))
+    return 1
 
-print (most_common_pair(tokens))
+
+if __name__ == "__main__":
+    sys.exit(main())

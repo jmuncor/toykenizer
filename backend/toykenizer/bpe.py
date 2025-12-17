2f7d36e73421aea1be708162e365706abc5c639a
Juan Munoz Corredor <jsmucor@gmail.com>
2025-12-16 21:39:09 -0800

    Created the decode funtion

--- backend/toykenizer/bpe.py
+++ backend/toykenizer/bpe.py
@@ -46,20 +46,52 @@ def mint_token(tokens: list[int], pair_to_replace: tuple[int, int]):
 
 
 #  Create the tokenizer and build it to a specific vocab_size
-def create(path: str = PATH_TO_FILE, vocab: int = VOCAB_SIZE):
+def create(path: str = PATH_TO_FILE, vocab_size: int = VOCAB_SIZE):
     tokens = get_tokens(path)
     new_tokens = list(tokens)
 
     merges = {}
-    for _ in range(vocab - 256):
+    for _ in range(vocab_size - 256):
         common_pair = most_common_pair(new_tokens)
         new_tokens, minted_token = mint_token(new_tokens, common_pair)
         merges[common_pair] = minted_token
 
-    return new_tokens, merges
+    return merges
+
+
+# Function to generate the full vocab using a dictionary of the new tokens
+def build_vocabulary(new_tokens: dict[tuple[int, int], int]) -> dict[int, bytes]:
+    vocab = {}
+    for i in range(256):
+        vocab[i] = bytes([i])
+
+    for (t0, t1), i in new_tokens.items():
+        vocab[i] = vocab[t0] + vocab[t1]
+
+    return vocab
+
+
+# Decode function using a vocabulary dict and a list of tokens
+def decode(tokens: list[int], vocab: dict[int, bytes]) -> str:
+    response = []
+    for i in tokens:
+        response.append(vocab[i])
+
+    response = b"".join(response)
+    response = response.decode("utf-8", errors="replace")
+
+    return response
+
+
+# Encode
+def encode(text: str, vocab: dict[int, bytes]) -> list[int]:
+    return []
 
 
 def main():
+    minted_tokens = create(PATH_TO_FILE, VOCAB_SIZE)
+    print(build_vocabulary(minted_tokens))
+
     return 1
 
 
